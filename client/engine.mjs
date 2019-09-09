ce5a885c111b06674aee0aed49b43de9d151691c
DooDesch <dennis.schmidt.1995@web.de>
2019-09-09 20:26:07 +0200

    Rewrite & using clearInterval now

--- client/engine.mjs
+++ client/engine.mjs
@@ -5,14 +5,15 @@ alt.log('Loaded: vehicle-mods->client/engine.mjs')
 
 let timeKeyPressed = 0
 let vehicle
-let engineIsRunning = false
+let interval
 
 alt.on('keydown', (key) => {
     if (key === 'F'.charCodeAt(0)) {
         if (alt.Player.local.vehicle) { // If the player is in a vehicle
-            timeKeyPressed = Date.now() // Get the actual time in milliseconds
             vehicle = alt.Player.local.vehicle.scriptID // Get the vehicle script ID
-            engineIsRunning = native.getIsVehicleEngineRunning(vehicle) // If the engine is running
+            if (native.getIsVehicleEngineRunning(vehicle)) { // If the engine is running
+                timeKeyPressed = Date.now() // Get the actual time in milliseconds
+            }
         }
     }
 })
@@ -21,21 +22,32 @@ alt.on('keyup', (key) => {
     if (key == 'F'.charCodeAt(0)) {
         if (timeKeyPressed > 0) { // If we're pressing the button and the player is in a vehicle
             timeKeyPressed = Date.now() - timeKeyPressed // Get the difference between key is pressed and released
-            if (timeKeyPressed > 500) { // If it's pressed for longer then 500ms
-                vehicle = null // Set vehicle to null, because we don't need it any longer
+            if (timeKeyPressed <= 500) { // If it's pressed for longer then 500ms
+                interval = alt.setInterval(turnEngineOn, 100)
                 return
             }
+            clear()
         }
     }
 })
 
-alt.setInterval(leaveVehicleEngineRunningOnExit, 200)
-
-function leaveVehicleEngineRunningOnExit() {
-    if (vehicle && timeKeyPressed <= 500 && !alt.Player.local.vehicle) { // If there's a vehicle & the key is pressed lesser than 500ms & the player isn't in a vehicle anymore
-        if (engineIsRunning) { // If the engine is running
-            native.setVehicleEngineOn(vehicle, true, true, false) // Set the engine on
-        }
-        vehicle = null // Remove the vehicle, because we don't need it anymore
+/**
+ * Sets the engine back on
+ */
+function turnEngineOn() {
+    if (vehicle && !alt.Player.local.vehicle) { // If there's a vehicle & the player isn't in a vehicle anymore
+        native.setVehicleEngineOn(vehicle, true, true, false) // Set the engine on
+        clear()
     }
+}
+
+/**
+ * Clear the interval, vehicle and timeKeyPressed variables
+ */
+function clear() {
+    if (interval)
+        alt.clearInterval(interval) // Clear the interval
+    
+    vehicle = null // Remove the vehicle, because we don't need it anymore
+    timeKeyPressed = 0 // Reset the variable
 }
